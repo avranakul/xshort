29419ae590a8a9fb125bb54f6f61bc8a5b8ac7eb
Nakul Saxena <74095699+avranakul@users.noreply.github.com>
2022-08-07 19:34:29 +0530

    Add files via upload

---
+++ X-Shorts ( NLP )/Article Summarizer.py
@@ -0,0 +1,120 @@
+# Importing the required packages for Summarizing
+import tkinter as tk
+import nltk
+from textblob import TextBlob
+from newspaper import Article
+
+def summarize(): # Programm for summarizing
+
+    url = utext.get('1.0', "end").strip()
+
+    article = Article(url)
+
+
+    article.download()
+    article.parse()
+    article.nlp()
+
+    # print(f'Title: {article.title}')
+    #
+    # print(f'Author: {article.authors}')
+    #
+    # print(f'Publish Date: {article.publish_date}')
+    #
+    # print(f'Summary: {article.summary}')
+
+    title.config(state='normal')
+    author.config(state='normal')
+    publication.config(state='normal')
+    summary.config(state='normal')
+    sentiment.configure(state='normal')
+
+    title.delete('1.0', 'end')
+    title.insert('1.0', article.title)
+
+
+    author.delete('1.0', 'end')
+    author.insert('1.0', article.authors)
+
+    publication.delete('1.0', 'end')
+    publication.insert('1.0', article.publish_date)
+
+    summary.delete('1.0', 'end')
+    summary.insert('1.0', article.summary)
+
+    analysis = TextBlob(article.text)
+    sentiment.delete('1.0', 'end')
+    sentiment.insert('1.0', f'Polarity: {analysis.polarity}, Sentiment: {"Positive" if analysis.polarity > 0 else "Negative" if analysis.polarity < 0 else "Neutral" }')
+
+    title.config(state='disabled')
+    author.config(state='disabled')
+    publication.config(state='disabled')
+    summary.config(state='disabled')
+    sentiment.configure(state='disabled')
+
+
+
+root = tk.Tk()
+root.title("X-Shorts")
+root.geometry('1200x600')
+
+
+tlable = tk.Label(root, text = "Title")
+tlable.pack()
+
+title = tk.Text(root, height=1, width=140)
+title.config(state='disabled',bg='#dddddd')
+title.pack()
+
+
+
+alable = tk.Label(root, text = "Author")
+alable.pack()
+
+author = tk.Text(root, height=1, width=140)
+author.config(state='disabled',bg='#dddddd')
+author.pack()
+
+
+
+plable = tk.Label(root, text = "Publication Date")
+plable.pack()
+
+publication = tk.Text(root, height=1, width=140)
+publication.config(state='disabled',bg='#dddddd')
+publication.pack()
+
+
+
+slabel= tk.Label(root, text = "Summary")
+slabel.pack()
+
+summary = tk.Text(root, height=19, width=140)
+summary.config(state='disabled',bg='#dddddd')
+summary.pack()
+
+
+
+
+selable = tk.Label(root, text = "Sentiment Analysis")
+selable.pack()
+sentiment = tk.Text(root, height=1, width=140)
+sentiment.config(state='disabled',bg='#dddddd')
+sentiment.pack()
+
+
+
+
+ulable = tk.Label(root, text = "URL")
+ulable.pack()
+
+utext= tk.Text(root, height=1, width=140)
+utext.pack()
+
+
+button = tk.Button(root, text="Summarize", command=summarize)
+button.pack()
+
+
+
+root.mainloop()
